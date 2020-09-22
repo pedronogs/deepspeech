0f4ea2b576575dad67f4bee4319d9961b51a93ff
pedronogs <phnogs@hotmail.com>
2020-09-22 14:00:04 -0300

    Running inference through ShellJS and bash scripting

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const path = require('path');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const multer  = require('multer');
-const spawn = require('child_process').spawnSync
 const fs = require('fs');
+const shell = require('shelljs');
 
+// Uses multer to store audio files
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads/')
@@ -17,16 +18,21 @@ var storage = multer.diskStorage({
     }
 });
 
+// Define upload variable for multer
 const upload = multer({ storage: storage });
 
+// Main route
 router.get('/',function(req, res){
     res.sendFile(path.join(__dirname + '/inference_application.html'));
 });
 
+// Route for uploading .wavfile and running inference
 router.post('/services/uploadAudioFile', upload.single('wavfile'), function(req, res) {
+    const { model, scorer } = req.body;
     const uploadedFile = req.file.path;
-
-    var consoleOutput = spawn('ls');
+    
+    // Runs inference
+    const { stdout, stderr } = shell.exec(`bash /home/pedronogs/Desktop/teste.sh ${model} ${scorer} audio.wav`);
 
     // Delete uploaded file after inference ran
     fs.unlink(uploadedFile, function (err) {
@@ -35,7 +41,8 @@ router.post('/services/uploadAudioFile', upload.single('wavfile'), function(req,
         }
     }); 
 
-    res.json({ "console": consoleOutput.output.toString("utf8") });
+    // Return json with inference output
+    res.json({ "output": stdout, "description_or_error": stderr });
 });
 
 // Add the router
